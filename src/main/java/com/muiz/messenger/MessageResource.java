65dacde4d1724fa4d08d21d74f7fcf9dc1178a97
muizzuddeen <muizzuddeen@gmail.com>
2017-05-22 19:21:34 +0100

    add messenger hello world with all dependencies

---
+++ src/main/java/com/muiz/messenger/MessageResource.java
@@ -0,0 +1,12 @@
+package com.muiz.messenger;
+
+/**
+ * Created by muizzuddeen on 18/05/2017.
+ */
+public class MessageResource {
+
+    public String getMessage() {
+        return "Hello World!!!";
+    }
+
+}
